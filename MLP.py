97811cbd775511bf9359a35e918d554fd05d6009
Boa <boajang97@gmail.com>
2023-04-21 00:48:12 +0900

    MLP

--- MLP.py
+++ MLP.py
@@ -1,13 +1,28 @@
 import torch
-import torch.nn as nn
+from torch import nn
 import torch.nn.functional as F
 
+torch.manual_seed(42) # Fix random seed
+
 class MLP(nn.Module):
-	def __init__(self,inputs):
+	def __init__(self):
 		super(MLP, self).__init__()
-		self.layer = nn.Linear(inputs, 1)
+		self.layer1 = nn.Linear(in_features=12, out_features=6)
+		self.layer2 = nn.Linear(in_features=6, out_features=1)
 		self.activation = nn.Sigmoid()
 	def forward(self, x):
-		x = F.ReLU(self.layer(x))
+		x = F.relu(self.layer1(x))
+		x = F.relu(self.layer2(x))
 		x = self.activation(x)
 		return x
+
+
+if __name__ == '__main__':
+	model = MLP()
+	print("     <<< Model Architecture >>>     ")
+	print(model)
+	print('-'*10)
+	x = torch.rand(12)
+	output = model(x)
+	print(f"Results: {output}")
+	print("Finish!")
