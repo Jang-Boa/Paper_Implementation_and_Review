e26806fcf0a05a99b0051011cb59a34108305f5a
Jang-Boa <boajang97@gmail.com>
2023-04-28 18:42:04 +0900

    resnet

--- ResNet.py
+++ ResNet.py
@@ -4,9 +4,13 @@ from torch.nn import functional as F
 
 torch.manual_seed(311)
 
-class ResNet(nn.Module):
+class ResNet(nn.Module):# 34-layer plain
     def __init__(self, ):
-
+        encoder = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(634, )
+        )
         return 
 
 if __name__ == '__main__':
