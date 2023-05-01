9cdcb93f78e2b517f2e10bdc91cb748b5d078c73
Boa <boajang97@gmail.com>
2023-05-02 02:19:34 +0900

    Update ResNet

--- ResNet.py
+++ ResNet.py
@@ -5,17 +5,17 @@ from torch.nn import functional as F
 torch.manual_seed(311)
 
 class BottleNeck(nn.Module):
-    """ BottleNeck의 핵심은 1x1 Conv. """
+    """ BottleNeck의 핵심은 1x1 Conv. ; Three-laer bottleneck block """
     expansion = 4
     def __init__(self, in_features, out_features, downsample=None, residual=True, stride=1):
         super(BottleNeck, self).__init__()
-        self.conv_1 = nn.Conv2d(in_channels=in_features, out_channels=out_features, kernel_size=1, stride=1, padding=0) # 1x1 conv
+        self.conv_1 = nn.Conv2d(in_channels=in_features, out_channels=out_features, kernel_size=1, stride=1, padding=0, bias=False) # 1x1 conv, reducing dimension
         self.bn_1 = nn.BatchNorm2d(out_features)
 
-        self.conv_2 = nn.Conv2d(in_channels=out_features, out_channels=out_features, kernel_size=3, stride=stride, padding=1) # 3x3 conv
+        self.conv_2 = nn.Conv2d(in_channels=out_features, out_channels=out_features, kernel_size=3, stride=stride, padding=1, bias=False) # 3x3 conv
         self.bn_2 = nn.BatchNorm2d(out_features)
     
-        self.conv_3 = nn.Conv2d(in_channels=out_features, out_channels=out_features * self.expansion, kernel_size=1, stride=1, padding=0) # 1x1 conv
+        self.conv_3 = nn.Conv2d(in_channels=out_features, out_channels=out_features * self.expansion, kernel_size=1, stride=1, padding=0, bias=False) # 1x1 conv, increasing dimension; 
         self.bn_3 = nn.BatchNorm2d(out_features * self.expansion)
         
         self.downsample = downsample
@@ -25,9 +25,9 @@ class BottleNeck(nn.Module):
     
     def forward(self, x):
         identity = x.clone() # copy tensor 
-        out = self.relu(self.bn_1(self.conv_1(x)))
+        out = self.relu(self.bn_1(self.conv_1(x))) 
         out = self.relu(self.bn_2(self.conv_2(out)))
-        out = self.bn_3(self.conv_3(out))
+        out = self.bn_3(self.conv_3(out)) # the output of each 3*3 layer, after BN and before other nonlinearity (ReLU/addition)
         if self.downsample is not None: # downsample 
             identity = self.downsample(identity)
         if self.residual is True: 
@@ -36,15 +36,16 @@ class BottleNeck(nn.Module):
         return out
 
 class Standard(nn.Module):
-    """ Standarc Block은 3x3 conv로 구성 """
+    """ Standarc Block은 3x3 conv. 2-layer block로 구성 
+    """
     expansion = 1 # global 
     def __init__(self, in_features, out_features, downsample=None, residual=True, stride=1):
         super(Standard, self).__init__()
         # BatchNorm에 bias가 포함되어 있으므로, conv2d는 bias = False호 설정
-        self.conv_1 = nn.Conv2d(in_channels=in_features, out_channels=out_features, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.conv_1 = nn.Conv2d(in_channels=in_features, out_channels=out_features, kernel_size=3, stride=stride, padding=1, bias=False) # stride -> 
         self.bn_1 = nn.BatchNorm2d(out_features)
         
-        self.conv_2 = nn.Conv2d(in_channels=out_features, out_channels=out_features*self.expansion , kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv_2 = nn.Conv2d(in_channels=out_features, out_channels=out_features*self.expansion , kernel_size=3, stride=1, padding=1, bias=False) 
         self.bn_2 = nn.BatchNorm2d(out_features*self.expansion)
         
         self.downsample = downsample
@@ -54,15 +55,13 @@ class Standard(nn.Module):
     
     def forward(self, x):
         identity = x.clone()
-        print(identity.shape)
         out = self.relu(self.bn_1(self.conv_1(x)))
         out = self.bn_2(self.conv_2(out))
-        if self.downsample is not None: 
+        if self.downsample is not None: # DownSampling의 진행 여부
             identity = self.downsample(identity)
-        if self.residual is True: 
-            out += identity
+        if self.residual is True: # Residual Block의 사용 여부 
+            out += identity # Shortcut connection : Identity Mapping (sufficient for address the degration problem)
         
-        print(out.shape)
         out = self.relu(out)
         return out
 
@@ -70,11 +69,11 @@ class ResNet(nn.Module):# 34-layer plain
     def __init__(self, Block, block_num, channel_size, num_channels=3, num_classes=1000, residual_block=True):
         super(ResNet, self).__init__()
         self.layer1 = nn.Sequential(
-            nn.Conv2d(num_channels, 64, kernel_size=7, stride=2, padding=1, bias=False),
+            nn.Conv2d(num_channels, 64, kernel_size=7, stride=2, padding=3, bias=False, padding_mode='zeros'),
             nn.BatchNorm2d(64),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
         )
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         self.in_features = channel_size[0]
         self.residual_block = residual_block
         self.layer2 = self._mask_layer(Block, num_layer=block_num[0], channel=channel_size[0])
@@ -95,7 +94,7 @@ class ResNet(nn.Module):# 34-layer plain
         if stride != 1 or self.in_features != channel*Block.expansion: 
             # projection mapping using 1x1 conv
             downsample = nn.Sequential(
-                nn.Conv2d(in_channels=self.in_features, out_channels=channel*Block.expansion, kernel_size=1, stride=stride), 
+                nn.Conv2d(in_channels=self.in_features, out_channels=channel*Block.expansion, kernel_size=1, stride=stride), # Projection shortcuts done by 1*1 conv.
                 nn.BatchNorm2d(channel*Block.expansion)
             )
         layers.append(Block(self.in_features, channel, downsample=downsample, residual=self.residual_block, stride=stride))
@@ -109,6 +108,7 @@ class ResNet(nn.Module):# 34-layer plain
     def forward(self, x):
         out = self.layer1(x)
         print(out.shape)
+        out = self.maxpool(out)
         out = self.layer2(out)
         print(out.shape)
         out = self.layer3(out)
@@ -119,12 +119,13 @@ class ResNet(nn.Module):# 34-layer plain
         print(out.shape)
         out = self.avgpool(out)
         out = torch.flatten(out, 1)
-        out = self.fc(out)
+        out = F.softmax(self.fc(out), dim=1)
         return out
 
 if __name__ == '__main__':
     x = torch.rand(2, 3, 224, 224)
-    model = ResNet(Block=Standard, block_num=[3, 4, 6, 3], channel_size=[64, 128, 256, 512], num_channels=3, num_classes=2, residual_block=True)
-    # print(model)
+    model = ResNet(Block=BottleNeck, block_num=[3, 4, 6, 3], channel_size=[64, 128, 256, 512], num_channels=3, num_classes=2, residual_block=True)
+    print(model)
     output = model(x)
+    print('-'*10)
     print(output)
