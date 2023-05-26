f64149c42492e4a423c9799db41bdd7f8d44db61
Boa <boajang97@gmail.com>
2023-05-27 02:35:06 +0900

    'checkpoint'

--- DenseNet.py
+++ DenseNet.py
@@ -5,40 +5,60 @@ from torchsummary import summary
 
 torch.manual_seed(311)
 
-class DenseBlock(nn.Module):
-    def __init__(self, in_feature, k):
-        super(DenseBlock, self).__init__()
-        self.bn = nn.BatchNorm2d(num_features=in_feature)
-        self.conv1 = nn.Conv2d(in_channels=in_feature, out_channels=k*4, kernel_size=1, stride=1)
-        self.conv2 = nn.Conv2d(in_channels=k*4, out_channels=k, kernel_size=3, stride=2, padding=1)
-        self.relu = nn.ReLU(inplace=True)
+# class DenseBlock(nn.Module):
+#     """DenseNet-BC structure with 4 dense blocks on 224 x 224 images """
+#     def __init__(self, in_feature, k):
+#         super(DenseBlock, self).__init__()
+#         self.bn = nn.BatchNorm2d(num_features=in_feature)
+#         self.conv1 = nn.Conv2d(in_channels=in_feature, out_channels=k*4, kernel_size=1, stride=1)
+#         self.conv2 = nn.Conv2d(in_channels=k*4, out_channels=k, kernel_size=3, stride=2, padding=1)
+#         self.relu = nn.ReLU(inplace=True)
         
+#     def forward(self, x):
+#         residual = x.clone()
+#         out = self.conv1(x)
+#         out = self.conv2(x)
+#         out = torch.cat([out, residual])
+#         return out
+
+class BottleneckLayer(nn.Module):
+    def __init__(self, in_feature, out_feature):
+        super(BottleneckLayer, self).__init__()
+        """ BN-ReLU-Conv1-BN-ReLU-Conv3
+        Each layer only produces k output feature maps """
+        self.bn1 = nn.BatchNorm2d(in_feature)
+        self.relu = nn.ReLu(inplace=True)
+        self.conv1 = nn.Conv2d(in_channels=in_feature, out_channels=out_feature, kernel_size=1, stride=1, padding=0)
+        self.bn2 = nn.BatchNorm2d(in_feature)
+        self.conv2 = nn.Conv2d(in_channels=in_feature, out_channels=out_feature, kernel_size=3, stride=1, padding=1)
+
     def forward(self, x):
-        residual = x.clone()
-        out = self.conv1(x)
-        out = self.conv2(x)
-        out = torch.cat([out, residual])
+        x_ = x.clone()
+        out = self.conv1(self.relu(self.bn1(x)))
+        out = self.conv2(self.relu(self.bn2(out)))
+        out = torch.cat([x_, out], 1)
         return out
 
+
 class TransitionLayer(nn.Module):
-    def __init__(self, in_feature):
+    """ transition layers consist of a batch normalization layer and an 1 x 1 convolutional layer followed by a 2x2 average pooling layer """
+    def __init__(self, in_feature, out_feature):
         super(TransitionLayer, self).__init__()
         self.bn = nn.BatchNorm2d(num_features=in_feature)
         self.relu = nn.ReLU(inplace=True)
-        self.conv = nn.Conv2d(in_channels=in_feature, out_channels=in_feature, kernel_size=1)
-        self.avgpool = nn.AvgPool2d(kernel_size=2, stride=2)
+        self.conv = nn.Conv2d(in_channels=in_feature, out_channels=out_feature, kernel_size=1, stride=1, padding=0)
+        self.avgpool = nn.AvgPool2d(kernel_size=2, stride=1)
     
     def forward(self, x):
         out = self.avgpool(self.conv(self.relu(self.bn(x))))
         return out
 
 class DenseNet(nn.Module):
-    def __init__(self, in_feature=3, out_feature=1000, dense_block=[6, 12, 24, 16], k=32):
+    def __init__(self, in_feature=3, num_classes=1000, dense_block=[6, 12, 24, 16], k=32):
         super(DenseNet, self).__init__()
         self.in_feature = in_feature
-        self.out_feature = out_feature
         self.dense_block = dense_block
-        self.k = k
+        self.k = 2*k # 1st channel 
         self.bn = nn.BatchNorm2d(in_channel=self.in_feature)
         self.relu = nn.ReLU(inplace=True)
         self.conv = nn.Conv2d(in_channels=self.in_feature, out_channels=k*2, kernel_size=7, stride=2, padding=1)
@@ -47,7 +67,7 @@ class DenseNet(nn.Module):
             self. 
         self.classifier = nn.Sequential(
             nn.AvgPool2d(kernel_size=7, stride=3), 
-            nn.Linear(in_features=2048, out_features=self.out_feature)
+            nn.Linear(in_features=2048, out_features=num_classes)
         )
     def _make_layer(self):
         dense_block = dict()
@@ -66,7 +86,7 @@ class DenseNet(nn.Module):
         return out
 
 if __name__ == '__main__':
-    x = torch.randn(3, 299, 299)
+    x = torch.randn(3, 224, 224)
     model = DenseNet()
     out = model(x)
-    summary(model, (3, 299, 299))
+    summary(model, (3, 224, 224))
