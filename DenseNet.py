e466f89c25f85c1b0ebf3c003c53ea2716ae5c7e
Jang-Boa <boajang97@gmail.com>
2023-05-31 04:59:20 +0900

    Almost done

--- DenseNet.py
+++ DenseNet.py
@@ -28,6 +28,7 @@ class DenseBlock(nn.Module):
             input_feature = in_feature + idx * growth_rate
             layer = DenseLayer(input_feature, growth_rate)
             self.add_module("denselayer%d"%(idx+1), layer)
+            
     def forward(self, init_feature):
         features = [init_feature]
         for name, layer in self.named_children():
@@ -38,9 +39,10 @@ class DenseBlock(nn.Module):
 class TransitionBlock(nn.Module):
     def __init__(self, in_feature, compression):
         super(TransitionBlock, self).__init__()
+        out_feature = int(in_feature*compression)
         self.bn = nn.BatchNorm2d(in_feature)
         self.relu = nn.ReLU(inplace=True)
-        self.conv = nn.Conv2d(in_channels=in_feature, out_channels=in_feature*compression, kernel_size=1, stride=1, padding=0, bias=False)
+        self.conv = nn.Conv2d(in_channels=in_feature, out_channels=out_feature, kernel_size=1, stride=1, padding=0, bias=False)
         self.pool = nn.AvgPool2d(kernel_size=2, stride=2, padding=0)
         
     def forward(self, x):
@@ -48,7 +50,7 @@ class TransitionBlock(nn.Module):
         return out
     
 class DenseNet(nn.Module):
-    def __init__(self, in_feature=3, out_feature=1000, num_dense_block=[6, 12, 24, 16], growth_rate=32, compression=0.5):
+    def __init__(self, in_feature=3, num_classes=1000, num_dense_block=[6, 12, 24, 16], growth_rate=32, compression=0.5):
         super(DenseNet, self).__init__()
         initial_feature = 2*growth_rate
         self.encoder = nn.Sequential(
@@ -62,9 +64,26 @@ class DenseNet(nn.Module):
         
         for idx, num_layer in enumerate(num_dense_block):
             self.encoder.add_module("denseblock%d"%(idx+1), DenseBlock(initial_feature, num_layer, growth_rate))
-            if idx != len(num_dense_block):
-                self.encoder.add_module("transition%d"%(idx+1), TransitionBlock())
+            initial_feature = initial_feature + num_layer * growth_rate
+            if idx != len(num_dense_block)-1:
+                self.encoder.add_module("transition%d"%(idx+1), TransitionBlock(initial_feature, compression))
+                initial_feature = initial_feature//2
+        
+        self.bn1 = nn.BatchNorm2d(initial_feature)
+        self.avgpool1 = nn.AvgPool2d(kernel_size=7, stride=3)
+        self.classifier = nn.Linear(in_features=initial_feature, out_features=num_classes, bias=True)
+        
     
     def forward(self, x):
         out = self.encoder(x)
+        out = self.avgpool1(self.bn1(out))
+        out = torch.flatten(out, 1)
+        out = self.classifier(out)
         return out
+    
+if __name__ == '__main__':
+    x = torch.randn(2, 3, 224, 224)
+    model = DenseNet()
+    out = model(x)
+    print(model)
+    print(out.shape)
