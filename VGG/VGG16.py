eda5a209fe995b84920c9e68c460ff3316010721
Boa <boajang97@gmail.com>
2023-06-04 22:30:56 +0900

    VGG

---
+++ VGG/VGG16.py
@@ -0,0 +1,153 @@
+import torch 
+from torch import nn
+from torch.nn import functional as F
+
+torch.manual_seed(311)
+
+class VGG16(nn.Module):
+    """ 
+    Base model of VGG16
+    """
+    def __init__(self, num_classes=10):
+        super(VGG16, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU())
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(), 
+            nn.MaxPool2d(kernel_size = 2, stride = 2))
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU())
+        self.layer4 = nn.Sequential(
+            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size = 2, stride = 2))
+        self.layer5 = nn.Sequential(
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU())
+        self.layer6 = nn.Sequential(
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU())
+        self.layer7 = nn.Sequential(
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size = 2, stride = 2))
+        self.layer8 = nn.Sequential(
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU())
+        self.layer9 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU())
+        self.layer10 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size = 2, stride = 2))
+        self.layer11 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU())
+        self.layer12 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU())
+        self.layer13 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size = 2, stride = 2))
+        self.fc = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(7*7*512, 4096),
+            nn.ReLU())
+        self.fc1 = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(4096, 4096),
+            nn.ReLU())
+        self.fc2= nn.Sequential(
+            nn.Linear(4096, num_classes))
+        
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = self.layer5(out)
+        out = self.layer6(out)
+        out = self.layer7(out)
+        out = self.layer8(out)
+        out = self.layer9(out)
+        out = self.layer10(out)
+        out = self.layer11(out)
+        out = self.layer12(out)
+        out = self.layer13(out)
+        out = out.reshape(out.size(0), -1)
+        out = self.fc(out)
+        out = self.fc1(out)
+        out = self.fc2(out)
+        return out
+
+##### CHANGE VERSION 1 #####
+class SimpleVGG16(nn.Module):
+    """
+    Simplized VGG16 architecture using for loops 
+    """
+    def __init__(self, in_features=3, num_classes=10):
+        super(SimpleVGG16, self).__init__()
+        self.in_features = in_features
+        self.num_classes = num_classes
+        layers = self.create_conv_block(self.in_features)
+        self.conv_block = nn.Sequential(*layers)
+        self.fc = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(512*7*7, 4096), 
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(4096, 4096),
+            nn.ReLU(),
+            nn.Linear(4096, self.num_classes)
+        )
+        
+    def forward(self, x):
+        x = self.conv_block(x)
+        x = torch.flatten(x, 1) # flatten으로 하면 안됨
+        x = self.fc(x)
+        # x = F.softmax(x, dim=0)
+        return x
+
+    def create_conv_block(self, in_features):
+        block = []
+        for channel in [64, 128, 256, 512, 512]:
+            out_features = channel
+            if (channel == 64) | (channel == 128):
+                num_loops = 2
+            else:
+                num_loops = 3
+            for loop in range(num_loops):
+                block += [nn.Conv2d(in_channels=in_features, out_channels=out_features, kernel_size=3, stride=1, padding=1),
+                         nn.BatchNorm2d(channel),
+                         nn.ReLU(),
+                         ]
+                in_features = channel
+            block += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        
+        return block
+
+if __name__ == '__main__':
+    x = torch.rand(3, 3, 224, 224) # 현재의 이슈: Batch size 1일 경우 가능, batch size 설정 어려움
+    model = SimpleVGG16()
+    print(model)
+    output = model(x)
+    print('-'*50)
+    print(output)
